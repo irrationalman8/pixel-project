22f9d9c09bbc570aca70870a4f1db5ebcbe5552a
Cody-Torlincasi <cody.tmusic@gmail.com>
2019-09-16 20:26:03 -0400

    Committing to submit project

---
+++ designs.js
@@ -0,0 +1,42 @@
+// Select color input
+// Select size input
+var height;
+var width;
+var color;
+// When size is submitted by the user, call makeGrid()
+
+// Storing submit form to a variable for easier use
+var input = document.querySelector("#sizePicker");
+//Adding listener for form submission
+input.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  height = document.querySelector('#inputHeight').value;
+  width = document.querySelector('#inputWidth').value;
+  makeGrid(height, width);
+  })
+
+//Store pixelCanvas id to a variable to make it easier to add rows in makeGrid
+var grid = document.querySelector('#pixelCanvas');
+
+
+function makeGrid(input1, input2) {
+  grid.innerHTML = "";
+  for (var i= 1; i <= input1; i ++) {
+    var row = document.createElement('tr');
+    grid.appendChild(row);
+    for (var x = 1; x <= input2; x++) {
+      var col = document.createElement('td');
+      row.appendChild(col);
+    }
+  }
+}
+
+
+//changing the color using the sizePicker
+
+grid.addEventListener('click', function() {
+  color = document.querySelector('#colorPicker').value;
+
+  event.target.style.backgroundColor = color;
+})
